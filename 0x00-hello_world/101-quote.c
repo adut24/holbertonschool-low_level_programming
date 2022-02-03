c99aa10367c3c1a04b2846a0d9524aed4e3b4164
adut24 <dutertre.alexandre@laposte.net>
2022-02-03 18:02:28 +0100

    priting without using printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: here 1 is expected
+ */
+int main(void)
+{
+	char cit[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(cit, sizeof(cit), 1, stdout);
+	return (1);
+}
