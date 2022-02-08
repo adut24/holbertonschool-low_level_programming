00c079403354a03cac644f2fdeb14baf773e8678
adut24 <dutertre.alexandre@laposte.net>
2022-02-08 13:06:17 +0100

    Add: program computing absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of an integer
+ * @n: integer value
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
