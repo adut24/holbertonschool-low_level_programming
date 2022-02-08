380eda33f0f9a3bd4121e49526dd6d7acaabf401
adut24 <dutertre.alexandre@laposte.net>
2022-02-08 11:14:29 +0100

    Add: program printing the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet ten times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int limite = 1;
+
+	for (int i = 0; i < 26; i++)
+	{
+
+		_putchar(alpha[i]);
+
+		if (i == 25)
+		{
+			_putchar('\n');
+			if (limite == 10)
+			{
+				i = 26;
+			}
+			else
+			{
+				i = -1;
+				limite++;
+			}
+		}
+	}
+}
