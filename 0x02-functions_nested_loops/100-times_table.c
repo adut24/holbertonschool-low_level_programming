ee02dc0a5e739f908d65b1da71b74ef527bdc469
adut24 <dutertre.alexandre@laposte.net>
2022-02-08 17:46:09 +0100

    Add: program printing the n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * print_times_table - prints times table
+ * @n:
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	if (n > 0 && n <= 15)
+	{
+		int i, j, resultat;
+
+		for (i = 0; i < n+1; i++)
+		{
+			_putchar('0');
+			_putchar(',');
+			for (j = 1; j < n+1; j++)
+			{
+				resultat = i * j;
+				if (resultat < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(resultat + '0');
+				}
+				else if (resultat >= 10 && resultat < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(resultat / 10 + '0');
+					_putchar(resultat % 10 + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(resultat / 100 + '0');
+					_putchar((resultat / 10) % 10 + '0');
+					_putchar(resultat % 10 + '0');
+				}
+				if (j != n)
+				{
+					_putchar(',');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
