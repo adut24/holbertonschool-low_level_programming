a8ff37616894c04330bfdb5527cc330d3c7d5a95
adut24 <dutertre.alexandre@laposte.net>
2022-02-08 10:19:37 +0100

    Add: program printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "_putchar.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char put[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(put[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
