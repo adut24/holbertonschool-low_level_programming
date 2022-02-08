4822e547204b21583ce09454c51857944c9ae261
adut24 <dutertre.alexandre@laposte.net>
2022-02-08 11:48:42 +0100

    Add: program checking for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: ASCII value
+ *
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
