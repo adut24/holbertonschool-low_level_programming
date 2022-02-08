ed456f20d5e57063c9738a13d08502cdd5b6c64a
adut24 <dutertre.alexandre@laposte.net>
2022-02-08 19:08:49 +0100

    Correction of the program

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,14 +6,16 @@
  */
 int main(void)
 {
-	int i;
+	int i, addition;
 
-	for(i = 0; i < 1024; i++)
+	addition = 0;
+	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			printf("%d\n", i);
+			addition += i;
 		}
 	}
+	printf("%d\n", addition);
 	return (0);
 }
