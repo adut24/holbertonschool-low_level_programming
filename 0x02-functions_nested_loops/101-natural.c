1ecbce4d3a49e22d97b9c8642007523b793f9805
adut24 <dutertre.alexandre@laposte.net>
2022-02-08 19:04:43 +0100

    Add: program printing multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			printf("%d\n", i);
+		}
+	}
+	return (0);
+}
