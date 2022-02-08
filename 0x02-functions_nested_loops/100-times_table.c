50bcacb2617794da89b32fb7ef4656f46b5d308b
adut24 <dutertre.alexandre@laposte.net>
2022-02-08 18:18:56 +0100

    Correction of the program

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,19 +7,19 @@
  */
 void print_times_table(int n)
 {
-	if (n > 0 && n <= 15)
+	if (n >= 0 && n <= 15)
 	{
 		int i, j, resultat;
 
 		for (i = 0; i < n + 1; i++)
 		{
 			_putchar('0');
-			_putchar(',');
 			for (j = 1; j < n + 1; j++)
 			{
 				resultat = i * j;
 				if (resultat < 10)
 				{
+					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
@@ -27,6 +27,7 @@ void print_times_table(int n)
 				}
 				else if (resultat >= 10 && resultat < 100)
 				{
+					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(resultat / 10 + '0');
@@ -34,22 +35,14 @@ void print_times_table(int n)
 				}
 				else
 				{
+					_putchar(',');
 					_putchar(' ');
 					_putchar(resultat / 100 + '0');
 					_putchar((resultat / 10) % 10 + '0');
 					_putchar(resultat % 10 + '0');
 				}
-				if (j != n)
-				{
-					_putchar(',');
-				}
 			}
 			_putchar('\n');
 		}
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		_putchar('\n');
 	}
 }
