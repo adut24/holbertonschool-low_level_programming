26b6bc536999791037d75e864f339200a387368f
adut24 <dutertre.alexandre@laposte.net>
2022-02-10 14:08:19 +0100

    Add: program print an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_number - print numbers
+ * @n: integer value
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar('0' + n % 10);
+}
