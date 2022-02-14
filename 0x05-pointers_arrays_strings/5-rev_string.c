ae6dc0e002e673a2105d1d7316c732df483477ff
adut24 <dutertre.alexandre@laposte.net>
2022-02-14 12:49:51 +0100

    Add: program reversing the string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: char value
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+/**
+ * @brief
+ *
+ */
+void rev_string(char *s)
+{
+	int length, i;
+	char *begin, *end, temp;
+
+	length = _strlen(s);
+	begin = s;
+	end = s;
+
+	for (i = 0; i < length - 1; i++)
+		end++;
+
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = *end;
+		*end = *begin;
+		*begin = temp;
+		begin++;
+		end--;
+	}
+}
