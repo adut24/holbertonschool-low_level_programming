e3119ba65d2467094f6e49ca5a24e01825e0211f
adut24 <dutertre.alexandre@laposte.net>
2022-02-14 15:04:35 +0100

    Correction

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,22 @@
 #include "main.h"
 /**
+ * _strlen - return the length of a string
+ * @s: char value
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+/**
  * *_strcpy - copy a string
  * @dest: destination of the string
  * @src: source of the string
@@ -8,13 +25,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i, length;
 
-	i = 0;
-	while (src[i] >= '\0')
+	length = _strlen(src);
+	for (i = 0; i < length; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
 	return (dest);
 }
