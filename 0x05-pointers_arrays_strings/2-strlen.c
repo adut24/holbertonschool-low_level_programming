e0cc522177889697ee9f7c17e2e52b9dcd1112bd
adut24 <dutertre.alexandre@laposte.net>
2022-02-14 10:52:43 +0100

    Add: program printing length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: char value
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
