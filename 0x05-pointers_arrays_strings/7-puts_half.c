4f5f748ba6f0fe383a34a339ff3ba07d2ed207f2
adut24 <dutertre.alexandre@laposte.net>
2022-02-14 14:03:50 +0100

    Correction

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -27,7 +27,7 @@ void puts_half(char *str)
 	length = _strlen(str);
 	if (length % 2 == 0)
 	{
-		for (i = (length +1) / 2; i < length; i++)
+		for (i = (length + 1) / 2; i < length; i++)
 			_putchar(str[i]);
 	}
 	else
