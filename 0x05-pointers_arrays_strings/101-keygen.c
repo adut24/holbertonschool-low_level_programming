22b8f9d55ee2eeffb1d36a9fed3c46b01fc5a3da
adut24 <dutertre.alexandre@laposte.net>
2022-02-15 14:40:47 +0100

    Add: program generating password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
 /**
  * main - generates random valid passwords
  *
- * Return: 0
+ * Return: string with value of 2772
  */
 int main(void)
 {
+	int i, nb, key;
+
+	srand(time(NULL));
+	for (i = 0; key < 2646; i++)
+	{
+		nb = rand() % 126 + 1;
+		key += nb;
+		printf("%c", nb);
+	}
+	nb = 2772 - key;
+	printf("%c", nb);
 	return (0);
 }
