867fd272166e7945c3d8db1560fbe91cb13d8c5c
adut24 <dutertre.alexandre@laposte.net>
2022-02-17 20:17:47 +0100

    Placeholder

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * *infinite_add - add two numbers
+ * @n1: number one
+ * @n2: number two
+ * @r: buffer used to store the result
+ * @size_r: size of the buffer
+ *
+ * Return: pointer to the result
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (r);
+}
