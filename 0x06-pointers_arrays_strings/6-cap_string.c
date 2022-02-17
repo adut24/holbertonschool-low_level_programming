e9aed5275a0abf805a30424db695855c70b5ead2
adut24 <dutertre.alexandre@laposte.net>
2022-02-17 10:52:32 +0100

    Correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,6 +19,8 @@ char *cap_string(char *str)
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= 32;
 		}
+		if (str[0] >= 'a' && str[0] <= 'z')
+			str[0] -= 32;
 	}
 	return (str);
 }
