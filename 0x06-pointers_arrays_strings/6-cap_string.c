6602b0a6d3a20b1d01fe63949438b96e094ce57d
adut24 <dutertre.alexandre@laposte.net>
2022-02-17 10:12:07 +0100

    Correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - return the length of a string
+ * @s: char value
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
 /**
  * *cap_string - convert first letter to uppercase
  * @str: string to convert
@@ -8,6 +26,7 @@
 char *cap_string(char *str)
 {
 	int i;
+	char last_ch;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
@@ -19,6 +38,9 @@ char *cap_string(char *str)
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= 32;
 		}
+		last_ch = str[i];
 	}
+	if (last_ch == '\n')
+		str[0] -= 32;
 	return (str);
 }
