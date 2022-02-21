4715bdbc904a86dd75bf8803a0b0bc19ca45a246
adut24 <dutertre.alexandre@laposte.net>
2022-02-21 19:09:20 +0100

    Update for betty

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * @brief
- *
+ * set_string - set the value of "s" to "to"
+ * @s: the double pointer that needs to change
+ * @to: the pointer to point to
  */
 void set_string(char **s, char *to)
 {
