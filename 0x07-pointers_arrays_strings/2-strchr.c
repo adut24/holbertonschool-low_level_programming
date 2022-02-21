68ccde53b87cd8687f04b96da93563897ce842e1
adut24 <dutertre.alexandre@laposte.net>
2022-02-21 11:02:58 +0100

    Add program locating a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_strchr - locate a character in a string
+ * @s: string to verify
+ * @c: character to check if present
+ *
+ * Return: pointer to the position of c or 0
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s; s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	return (0);
+}
