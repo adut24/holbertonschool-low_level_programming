2d6bf2467a96381c182d70427cfb180a92ec6d4c
adut24 <dutertre.alexandre@laposte.net>
2022-02-23 17:41:50 +0100

    Simplification of the program

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,7 @@
  */
 int _strlen_recursion(char *s)
 {
-	int length = 0;
-
-	if (*s != '\0')
-	{
-		length++;
-		length = _strlen_recursion(s + 1) + 1;
-	}
-	return (length);
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
 }
