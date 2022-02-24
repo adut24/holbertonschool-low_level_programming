901bf331eb8da67bbdc25c5c09d4709003317084
adut24 <dutertre.alexandre@laposte.net>
2022-02-24 10:15:39 +0100

    Compare 2 strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,9 +8,13 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0')
-		return (0);
-	if (*s2 == '*' &&)
-		wildcmp(s + 1, s);
-	return (wildcmp(s1 + 1, s2 + 2));
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
