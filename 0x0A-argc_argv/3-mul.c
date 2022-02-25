bd6fcbee80b1803a06671e6a939afb7b56105173
adut24 <dutertre.alexandre@laposte.net>
2022-02-25 12:06:48 +0100

    Correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		printf("%d\n", atoi(argv[argc - 2]) * atoi(argv[argc - 1]));
-		return (0);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
+	return (0);
 }
