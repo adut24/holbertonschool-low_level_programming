4b5cee71021aaa071c6d309a4fdf37015ab186e2
adut24 <dutertre.alexandre@laposte.net>
2022-03-03 13:58:11 +0100

    Now copy correctly

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,15 +3,15 @@
 /**
  * _realloc - reallocate a block of memory
  * @ptr: pointer to reallocate
- * @old_size: old size of the pointer
- * @new_size: new size of the pointer
+ * @old_size: old size of the content of the pointer
+ * @new_size: new size of the content of the pointer
  *
  * Return: pointer
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
-	void *p;
+	char *p;
 
 	if (new_size == old_size)
 		return (ptr);
@@ -25,9 +25,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (old_size > new_size)
 	{
 		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
 		for (i = 0; i < new_size; i++)
 		{
-			p[i] = ptr[i];
+			p[i] = ((char *)ptr)[i];
 		}
 		free(ptr);
 		return (p);
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @brief
+ *
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+}
