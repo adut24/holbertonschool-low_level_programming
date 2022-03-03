cec8c0eb0f681211504c40a4bcf31c9f9cd14486
adut24 <dutertre.alexandre@laposte.net>
2022-03-03 12:50:36 +0100

    Correction for betty and pointer

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,19 +1,24 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * @brief
+ * array_range - print an array going from min to max
+ * @min: integer
+ * @max: integer
  *
+ * Return: pointer to the array
  */
 int *array_range(int min, int max)
 {
-	int *ar, i;
+	int *ar, *tmp, i;
 
 	if (min > max)
 		return (NULL);
 	ar = malloc((max + 1) * sizeof(int));
 	if (ar == NULL)
 		return (NULL);
+	tmp = ar;
 	for (i = 0; i <= max; i++)
 		ar[i] = min + i;
+	ar = tmp;
 	return (ar);
 }
