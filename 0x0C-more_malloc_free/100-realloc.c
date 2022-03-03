764873630cae641ac7c28a76af41e4fb4a77b47d
adut24 <dutertre.alexandre@laposte.net>
2022-03-03 13:36:12 +0100

    Correction

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,6 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		ptr = malloc(new_size);
 	}
-	ptr = malloc(new_size);
+	if (new_size > old_size)
+		ptr = malloc(old_size + (new_size - old_size));
 	return (ptr);
 }
