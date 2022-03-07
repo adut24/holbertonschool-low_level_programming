424f0104bb094eb60b44f6adee701a927bd5c498
adut24 <dutertre.alexandre@laposte.net>
2022-03-07 14:30:38 +0100

    Correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -52,7 +52,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
 
-	new = malloc(sizeof(dog_t));
+	new = malloc(sizeof(*new));
 	if (new == NULL)
 		return (NULL);
 	new->age = age;
