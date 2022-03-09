e462a25dcce668329082215807c3453c965e3f2b
adut24 <dutertre.alexandre@laposte.net>
2022-03-09 12:01:35 +0100

    Perform simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int (*ptr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	ptr = get_op_func(argv[2]);
+	if (ptr == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", ptr(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - add 2 integers
+ * @a: integer one
+ * @b: integer two
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtract 2 integers
+ * @a: integer one
+ * @b: integer two
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply 2 integers
+ * @a: integer one
+ * @b: integer two
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide 2 integers
+ * @a: integer one
+ * @b: integer two
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - divide 2 integers and take the rest of the division
+ * @a: integer one
+ * @b: integer two
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
