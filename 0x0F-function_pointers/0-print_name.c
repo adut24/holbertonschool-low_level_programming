ec697c653059895f6dcceb1d08fcd3ba9b70c158
adut24 <dutertre.alexandre@laposte.net>
2022-03-09 09:29:13 +0100

    Print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - print a name
+ * @name: string of the name to print
+ * @f: pointer to the function to use
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
