ed2bf52e72f3d5da212762d8de553cfd04c17725
adut24 <dutertre.alexandre@laposte.net>
2022-03-09 15:02:47 +0100

    Print the opcodes of the main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char **argv)
 {
-	int i, nb = atoi(argv[1]);
+	int i, nb;
 	char *str = (char *)main;
 
 	if (argc != 2)
@@ -16,6 +16,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(1);
 	}
+	nb = atoi(argv[1]);
 	if (nb < 0)
 	{
 		printf("Error\n");
@@ -23,7 +24,7 @@ int main(int argc, char **argv)
 	}
 	for (i = 0; i < nb; i++, str++)
 	{
-		printf("%02hhx", *str);
+		printf("%02x", *str & 0xff);
 		if (i != nb - 1)
 			printf(" ");
 		else
