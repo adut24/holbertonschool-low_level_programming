ec0b928b36537ba9a3c160907b0457b3a11453aa
adut24 <dutertre.alexandre@laposte.net>
2022-03-09 12:22:16 +0100

    Correction for array == NULL and cmp == NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,17 +8,20 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int comp, i;
-
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++, array++)
+	if (array != NULL && cmp != NULL)
 	{
-		comp = (*cmp)(*array);
-		if (comp != 0)
-			break;
+		int comp, i;
+
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++, array++)
+		{
+			comp = (*cmp)(*array);
+			if (comp != 0)
+				break;
+		}
+		if (i == size)
+			return (-1);
+		return (i);
 	}
-	if (i == size)
-		return (-1);
-	return (i);
 }
