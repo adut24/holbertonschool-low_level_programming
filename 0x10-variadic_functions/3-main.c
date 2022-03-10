051ad12a8570d7d1a6436e70a9b098c6151a932c
adut24 <dutertre.alexandre@laposte.net>
2022-03-10 11:50:29 +0100

    Print anything

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_all("pciskfs", 'B', 3, "stSchool", 3.907, NULL);
+	return (0);
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_all - print all the parameters
+ * @format: string of character telling the format of the parameters in order
+ */
+void print_all(const char * const format, ...)
+{
+	char *str, *separator;
+	int i = 0;
+	va_list ptr;
+
+	separator = "";
+	va_start(ptr, format);
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(ptr, int));
+			break;
+			case 'i':
+				printf("%s%d", separator, va_arg(ptr, int));
+			break;
+			case 'f':
+				printf("%s%f", separator, va_arg(ptr, double));
+			break;
+			case 's':
+				str = va_arg(ptr, char *);
+				if (str == NULL)
+					printf("%s(nil)", separator);
+				if (str != NULL)
+					printf("%s%s", separator, str);
+			break;
+			default:
+				i++;
+				continue;
+			break;
+		}
+		i++;
+		separator = ", ";
+	}
+	printf("\n");
+	va_end(ptr);
+}
