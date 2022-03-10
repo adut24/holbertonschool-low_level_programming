381f0fae87076a343bfba03c02b4834855ef17e7
adut24 <dutertre.alexandre@laposte.net>
2022-03-10 13:17:09 +0100

    Correction in case separator == NULL

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,17 +7,19 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	va_list ptr;
-
-	va_start(ptr, n);
-	for (i = 0; i < n; i++)
+	if (separator != NULL)
 	{
-		printf("%d", va_arg(ptr, int));
-		if (i != n - 1 && separator != NULL)
-			printf("%s", separator);
-		else if (i == n - 1)
-			printf("\n");
+		unsigned int i;
+		va_list ptr;
+
+		va_start(ptr, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(ptr, int));
+			if (i != n - 1 && separator != NULL)
+				printf("%s", separator);
+		}
+		va_end(ptr);
 	}
-	va_end(ptr);
+	printf("\n");
 }
