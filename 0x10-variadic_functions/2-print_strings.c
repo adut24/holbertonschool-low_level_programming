1c5db32e584ea02f84ba231f6aed2786c9516a0c
adut24 <dutertre.alexandre@laposte.net>
2022-03-10 13:21:57 +0100

    Correction in case n == 0

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,22 +7,24 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	va_list ptr;
-	char *str;
-
-	va_start(ptr, n);
-	for (i = 0; i < n; i++)
+	if (n != 0)
 	{
-		str = va_arg(ptr, char *);
-		if (str != NULL)
-			printf("%s", str);
-		else
-			printf("(nil)");
-		if (i != n - 1 && separator != NULL)
-			printf("%s", separator);
-		else if (i == n - 1)
-			printf("\n");
+		unsigned int i;
+		va_list ptr;
+		char *str;
+
+		va_start(ptr, n);
+		for (i = 0; i < n; i++)
+		{
+			str = va_arg(ptr, char *);
+			if (str != NULL)
+				printf("%s", str);
+			else
+				printf("(nil)");
+			if (i != n - 1 && separator != NULL)
+				printf("%s", separator);
+		}
+		va_end(ptr);
 	}
-	va_end(ptr);
+	printf("\n");
 }
