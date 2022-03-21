6e7574233a22d38dd32dcac62326cd89a9cc8e7e
adut24 <dutertre.alexandre@laposte.net>
2022-03-21 13:07:21 +0100

    Free a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a linked list
+ * @head: list to free
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *node;
+
+	node = *head;
+	while (node)
+	{
+		temp = node->next;
+		free(node);
+		node = temp;
+	}
+	*head = NULL;
+}
