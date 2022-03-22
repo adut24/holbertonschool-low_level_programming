b59c6abebc278e6b94185775263d97119d58729e
adut24 <dutertre.alexandre@laposte.net>
2022-03-22 08:57:17 +0100

    Correction for betty style

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 	listint_t *node, *temp;
 
-	if(!head)
+	if (!head)
 		return (-1);
 	if (!*head)
 		return (-1);
