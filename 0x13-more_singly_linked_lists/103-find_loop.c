3df4b2385d1f9cc63d77473be24ff73288dd7fb0
adut24 <dutertre.alexandre@laposte.net>
2022-03-22 17:11:32 +0100

    Correction of the node returned

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -23,9 +23,8 @@ listint_t *find_listint_loop(listint_t *head)
 			{
 				slow = slow->next;
 				fast = fast->next->next;
-				if (slow == fast)
-					return (slow);
 			}
+			return (slow);
 		}
 	}
 	return (NULL);
