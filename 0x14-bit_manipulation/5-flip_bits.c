05410882ae31b858d1388c89bca1726e6d3f9959
adut24 <dutertre.alexandre@laposte.net>
2022-03-24 13:45:41 +0100

    Find the number of bits different between two number

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * flip_bits - find the number of bit different between n and m
+ * @n: unsigned long
+ * @m: unsigned long
+ * Return:
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int nb = 0, i;
+
+	for (i = 0; i < 64; i++)
+	{
+		if (((n >> i) & 1) != ((m >> i) & 1))
+			nb++;
+	}
+	return (nb);
+}
