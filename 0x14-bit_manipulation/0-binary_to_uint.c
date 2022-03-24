56b4aa3986e83a03ea0febe59744a88425095e49
adut24 <dutertre.alexandre@laposte.net>
2022-03-24 10:49:30 +0100

    Convert binary to a number in base 10

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert a string of 0 and 1 to a number
+ * @b: string to convert
+ * Return: converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned nb = 0;
+
+	if (!b)
+		return (0);
+
+	for (; *b; b++)
+	{
+		nb *= 2;
+		if (*b >= '0' && *b <= '9')
+			nb += *b - '0';
+		else
+			return (0);
+	}
+	return (nb);
+}
