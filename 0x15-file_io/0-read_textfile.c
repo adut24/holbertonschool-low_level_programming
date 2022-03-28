13cb9481f66179be8715745c55df93b6ba6670b8
adut24 <dutertre.alexandre@laposte.net>
2022-03-28 10:57:34 +0200

    Read a text file and print it

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile -read a text file and print it to the standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to print
+ * Return: 0 or actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t i;
+	char *buffer = malloc(letters);
+	int w_suc, fd;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	read(fd, buffer, letters);
+
+	for (i = 0; i < letters; i++, buffer++)
+	{
+		w_suc = write(STDOUT_FILENO, buffer, 1);
+		if (w_suc == -1)
+			return (0);
+		if (*buffer == '\0')
+			break;
+	}
+
+	return (i);
+}
