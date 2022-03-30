4441eb000db9d11f977abcefb204e861cf8b55eb
adut24 <dutertre.alexandre@laposte.net>
2022-03-30 13:58:09 +0200

    Add two strings composed of numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,19 +1,15 @@
 #include "main.h"
 /**
  * _strlen - return the length of a string
- * @s: char value
- *
+ * @s: string
  * Return: length of the string
  */
 int _strlen(char *s)
 {
-	int length;
+	int length = 0;
 
-	length = 0;
 	while (s[length] != '\0')
-	{
 		length++;
-	}
 	return (length);
 }
 /**
@@ -22,16 +18,44 @@ int _strlen(char *s)
  * @n2: number two
  * @r: buffer used to store the result
  * @size_r: size of the buffer
- *
  * Return: pointer to the result
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int length;
+	int l1 = _strlen(n1), l2 = _strlen(n2), ret = 0, res, i, tmp;
 
-	length = _strlen(*r);
-	if (length > 509)
-		return ('0');
-	else
-		return (*r);
+	tmp = (*n1 - '0') + (*n2 - '0');
+	if ((l1 >= size_r - 1 && tmp > 9) || (l2 >= size_r - 1 && tmp > 9)
+	 || (l1 == size_r) || (l2 == size_r))
+		return (0);
+	while (*(n1 + 1) != '\0')
+		n1++;
+	while (*(n2 + 1) != '\0')
+		n2++;
+
+	r[size_r - 1] = '\0';
+	for (i = size_r - 2; *n1 && *n2; n1--, n2--, i--)
+	{
+		res = (*n1 - '0') + (*n2 - '0') + ret;
+		ret = res / 10;
+		r[i] = res % 10 + '0';
+	}
+	for (; *n1; n1--, i--)
+	{
+		res = (*n1 - '0') + ret;
+		ret = res / 10;
+		r[i] = res % 10 + '0';
+	}
+	for (; *n2; n2--, r--)
+	{
+		res = (*n1 - '0') + ret;
+		ret = res / 10;
+		r[i] = res % 10 + '0';
+	}
+	if (ret > 0)
+	{
+		r[i] = '1';
+		return (r + i);
+	}
+	return (r + i + 1);
 }
