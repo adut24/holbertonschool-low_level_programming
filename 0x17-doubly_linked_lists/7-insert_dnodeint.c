a392a7caaf113d4bab1bddf42834660ad1076b3e
adut24 <dutertre.alexandre@laposte.net>
2022-05-05 13:07:45 +0200

    Correction for prev

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	a = temp->next;
-	temp->next->prev = node;
+	if (temp->next)
+		temp->next->prev = node;
 	temp->next = node;
 	node->n = n;
 	node->next = a;
