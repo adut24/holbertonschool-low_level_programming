c53fd226993d45439f3a8b9e939280fac36bb9d2
adut24 <dutertre.alexandre@laposte.net>
2022-07-15 16:35:10 +0200

    Return the perimeter of the island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Island perimeter module"""
+
+def island_perimeter(grid):
+    """Return the perimeter of the island in grid"""
+    value = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    value += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    value += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    value += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    value += 1
+    return value
+
